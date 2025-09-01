f386eda3914ff279f13acbc38812021edb37cd3a
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-09-01 18:20:12 -0300

    feat(controller): Adiciona endpoint de cadastro e habilita Feign

--- src/main/java/br/com/guilhermevieira/clientes/ApiCadastroClientesApplication.java
+++ src/main/java/br/com/guilhermevieira/clientes/ApiCadastroClientesApplication.java
@@ -2,8 +2,10 @@ package br.com.guilhermevieira.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiCadastroClientesApplication {
 
 	public static void main(String[] args) {
